9ae85f9c1cb43b20faffa67513c6e946ed1f6e31
jakesipe123 <60919213+jakesipe123@users.noreply.github.com>
2020-11-06 17:21:17 +0800

    Add files via upload

---
+++ index8.php
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 25.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:rgb(255, 255, 255);clip-rule:evenodd; }
+</style>
+<g>
+	<path id="path10000" class="st0" d="M465.8,198.82C443.36,159.44,430.61,112.35,425,67c37.47,6.02,75.84,19.3,115.8,45.01l-1.29-28.97
+		c28.39,4.67,65.47,23.8,108.55,52.94l0.25-27.68c28.64,12.38,57.28,32.18,85.92,60.93l9.53-22.95
+		c26.36,15.7,53.65,43.04,81.95,82.31l10.03-24.02c22.24,20.37,40.8,50.24,54,93.96l15.14-17.16
+		c12.71,19.75,22.58,50.58,27.94,99.19l19.56-12.15c4.37,26.89-1.79,65.71-13.05,110.27l22.83-1.07
+		c-10.62,41.97-25.41,78.15-47.25,104.76c31.02,34.71,72.32,62.9,120.11,87.09c-1.24,19.58-7.3,33.98-17.92,43.55
+		c-54.85-42.31-106.81-87.94-157.49-134.97c-43.53-42.2-85.47-85.57-125.97-130.02c-71.97-78.88-138.58-160.18-201.41-248.34
+		c-18.66-26.16-36.78-52.72-54.25-79.67c0.99,17.16,50.83,97.73,86.21,147.97c92.57,131.54,200.67,246.77,309.17,360.86
+		c-28.34,26.33-56.93,41.69-85.87,45.35v-24.14c-34.74,13.17-68.45,22.5-97.13,13.62l10.97-22.56
+		c-30.13-1.18-63.03-12.15-88.55-29.54l15.78-19.47c-33.7-11.36-62.19-29.65-81.35-60.31l20.6-10.91
+		c-28.89-26.11-56.68-53.28-72.76-92.55l20.95-11.25c-25.31-33.36-45.27-65.88-53.55-96.49l25.07-1.18
+		c-25.66-45.01-42.69-86.53-47.7-123.33L465.8,198.82L465.8,198.82z"/>
+
+
+  <text y="40" font-size="30" fill="green">
+    <textPath href="#path10000">
+      “Tears were dripping onto my dress, but I wasn't making any sound. There was no sound to express thid kind of pain.
+I didn't want to move, didn't want to do anything. Fang was not waiting for me out in the living room. Tomorrow morning, when I woke up, Fang would still be gone.”
+― James Patterson, Fang
+    </textPath>
+  </text>
+    
+     
+ </g>
+</svg>
